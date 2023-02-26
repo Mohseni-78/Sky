b54f5e46ea5459866d5b9ebd7ed1249b796e23c1
Alireza Mohseni <mohseni.arz@gmail.com>
2023-02-26 17:09:48 +0330

    create a footer commponet in app

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import { useEffect } from 'react';
+
+// Imported Icons
+import { SiSkyliner } from "react-icons/si";
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaPinterestP } from "react-icons/fa";
+import { AiOutlineTwitter, AiFillYoutube } from "react-icons/ai";
+// Imported Aos
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Footer = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="sectionContainer container grid">
+        <div data-aos="fade-up" data-aos-duration='2500' className="gridOne">
+          <div className="logoDiv">
+            <SiSkyliner className="logo" />
+            {/* <img src="" alt="" /> */}
+          </div>
 
-export default Footer
+          <p>you mind should be stronger than your feelings, fly!</p>
+          <div className="socialIcons flex">
+            <TiSocialFacebook className="icon" />
+            <AiOutlineTwitter className="icon" />
+            <AiFillYoutube className="icon" />
+            <FaPinterestP className="icon" />
+          </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration='2500' className="footerLinks">
+          <span className="linkTitle">Information</span>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Explore</a>
+          </li>
+          <li>
+            <a href="">Flight Status</a>
+          </li>
+          <li>
+            <a href="">Travel</a>
+          </li>
+          <li>
+            <a href="">Check-in</a>
+          </li>
+          <li>
+            <a href="">Manage your booking</a>
+          </li>
+        </div>
+        <div data-aos="fade-up" data-aos-duration='2500' className="footerLinks">
+          <span className="linkTitle">Information</span>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Explore</a>
+          </li>
+          <li>
+            <a href="">Flight Status</a>
+          </li>
+          <li>
+            <a href="">Travel</a>
+          </li>
+          <li>
+            <a href="">Check-in</a>
+          </li>
+          <li>
+            <a href="">Manage your booking</a>
+          </li>
+        </div>
+        <div data-aos="fade-up" data-aos-duration='2500' className="footerLinks">
+          <span className="linkTitle">Information</span>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Explore</a>
+          </li>
+          <li>
+            <a href="">Flight Status</a>
+          </li>
+          <li>
+            <a href="">Travel</a>
+          </li>
+          <li>
+            <a href="">Check-in</a>
+          </li>
+          <li>
+            <a href="">Manage your booking</a>
+          </li>
+        </div>
+      </div>
+      <div className="copyRightDiv flex">
+        <p>
+          Courtesy Design | Developed by <a href="">IsraTech</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

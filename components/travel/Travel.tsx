69c97fb348a75e5a0a506c9ccb8f7efe17eadf83
Alireza Mohseni <mohseni.arz@gmail.com>
2023-03-17 16:32:33 +0330

    add travel com in add and style that

---
+++ components/travel/Travel.tsx
@@ -0,0 +1,84 @@
+import { useEffect } from 'react';
+import Image from 'next/image'
+// Imported Images
+import travel_img1 from "@/public/images/travel_img1.jpg";
+import travel_img2 from "@/public/images/travel_img2.jpg";
+import travel_img3 from "@/public/images/travel_img3.jpg";
+import travel_img4 from "@/public/images/travel_img4.jpg";
+import avatar from "@/public/images/avatar.jpg";
+import avatar2 from "@/public/images/avatar2.jpg";
+import avatar3 from "@/public/images/avatar3.png";
+import avatar4 from "@/public/images/avatar4.jpg";
+// Imported Aos
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Travel = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
+  return (
+    <div className="traveles container section">
+      <div className="sectionContainer">
+        <h2>top traveles of this month</h2>
+
+        <div className="travelesContainer grid">
+
+          <div  data-aos="fade-up" data-aos-duration='2500' className="singleTraveler">
+            <Image src={travel_img1} alt="" className="destinationImag" />
+            <div className="travelerDetails">
+              <div className="travelerPicture">
+                <Image src={avatar} alt="" className="travelerImage" />
+              </div>
+              <div className="travelerName">
+                <span>IsraTech</span>
+                <p>@IsraTech8</p>
+              </div>
+            </div>
+          </div>
+
+          <div data-aos="fade-up" data-aos-duration='2500' className="singleTraveler">
+            <Image src={travel_img2} alt="" className="destinationImag" />
+            <div className="travelerDetails">
+              <div className="travelerPicture">
+                <Image src={avatar2} alt="" className="travelerImage" />
+              </div>
+              <div className="travelerName">
+                <span>IsraTech</span>
+                <p>@IsraTech8</p>
+              </div>
+            </div>
+          </div>
+
+          <div data-aos="fade-up" data-aos-duration='2500' className="singleTraveler">
+            <Image src={travel_img3} alt="" className="destinationImag" />
+            <div className="travelerDetails">
+              <div className="travelerPicture">
+                <Image src={avatar3} alt="" className="travelerImage" />
+              </div>
+              <div className="travelerName">
+                <span>IsraTech</span>
+                <p>@IsraTech8</p>
+              </div>
+            </div>
+          </div>
+
+          <div data-aos="fade-up" data-aos-duration='2500' className="singleTraveler">
+            <Image src={travel_img4} alt="" className="destinationImag" />
+            <div className="travelerDetails">
+              <div className="travelerPicture">
+                <Image src={avatar4} alt="" className="travelerImage" />
+              </div>
+              <div className="travelerName">
+                <span>IsraTech</span>
+                <p>@IsraTech8</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Travel;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Search from '@/components/search/Search'
 import Support from '@/components/support/Support'
 import Info from '@/components/info/Info'
 import Lounge from '@/components/lounge/Lounge'
+import Travel from '@/components/travel/Travel'
 export default function Home() {
   return (
     <>
@@ -13,6 +14,7 @@ export default function Home() {
       <Support/>
       <Info/>
       <Lounge/>
+      <Travel/>
     </>
   )
 }
